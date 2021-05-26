41d0dfc542b653b4846f184c9dff0dacb2417683
patlevin <levin.patrick@gmail.com>
2021-05-26 02:27:07 +0200

    refactor color transfer method access

--- StyleTransfer/StyleTransferEffect/Color/TransferMethods.cs
+++ StyleTransfer/StyleTransferEffect/Color/TransferMethods.cs
@@ -7,53 +7,62 @@ using System.Linq;
 namespace PaintDotNet.Effects.ML.StyleTransfer.Color
 {
     /// <summary>
+    /// Descriptor of a color transfer method.
+    /// </summary>
+    public class ColorTransferDescriptor
+    {
+        public ColorTransferDescriptor(string name, string description)
+        {
+            Name = name;
+            Description = description;
+            DisplayName = name;
+        }
+
+        public ColorTransferDescriptor(
+            TransferMethodAttribute attr,
+            IColorTransfer instance)
+        {
+            Name = attr.Name;
+            DisplayName = Name;
+            Description = attr.Description;
+            Interface = instance;
+        }
+
+        /// <summary>
+        /// Name of the method.
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Localized display name of the method.
+        /// </summary>
+        public string DisplayName { get; set; }
+
+        /// <summary>
+        /// Description of the method.
+        /// </summary>
+        public string Description { get; private set; }
+
+        /// <summary>
+        /// Instance of the method.
+        /// </summary>
+        public IColorTransfer Interface { get; private set; }
+    }
+
+    /// <summary>
     /// Helpers for accessing all classes marked as transfer methods. 
     /// </summary>
     public static class TransferMethods
     {
         /// <summary>
-        /// Return the names of all transfer methods.
+        /// Descriptors of all available color transfer methods.
         /// </summary>
-        public static IEnumerable<string> AllNames => LoadTypes();
+        public static IReadOnlyList<ColorTransferDescriptor> All => LoadTypes();
 
-        /// <summary>
-        /// Return the description of a transfer method given its name.
-        /// </summary>
-        /// <param name="name">Name of the transfer method</param>
-        /// <returns>Description that matches the method name.</returns>
-        public static string GetDescription(string name)
-        {
-            if (types.TryGetValue(name, out Type type))
-            {
-                return ((TransferMethodAttribute)Attribute.GetCustomAttribute(
-                    type, typeof(TransferMethodAttribute))).Description;
-            }
-            return null;
-        }
-
-        /// <summary>
-        /// Return an instance of a transfer method given its name.
-        /// </summary>
-        /// <param name="name">Name ofthe method.</param>
-        /// <returns>Instance of the transfer method; <c>null</c> if no matching method could be found.</returns>
-        public static IColorTransfer GetInstance(string name)
-        {
-            if (types.TryGetValue(name, out Type type))
-            {
-                var ctor = type.GetConstructor(new Type[0]);
-                return ctor.Invoke(new object[0]) as IColorTransfer;
-            }
-            return null;
-        }
-
-        private static IEnumerable<string> LoadTypes()
+        private static IReadOnlyList<ColorTransferDescriptor> LoadTypes()
         {
             if (types.Count == 0)
             {
-                (Type, Attribute) TypeAttributePair(Type type)
-                {
-                    return (type, Attribute.GetCustomAttribute(type, typeof(TransferMethodAttribute)));
-                }
 
                 var typeList = typeof(TransferMethods).Assembly.GetExportedTypes();
                 var transforms = typeList.Select(TypeAttributePair)
@@ -61,14 +70,28 @@ namespace PaintDotNet.Effects.ML.StyleTransfer.Color
 
                 foreach (var (type, attr) in transforms)
                 {
-                    types.Add(((TransferMethodAttribute)attr).Name, type);
+                    var inst = GetInstance(type);
+                    types.Add(new ColorTransferDescriptor(attr, inst));
+                }
+
+                (Type, TransferMethodAttribute) TypeAttributePair(Type type)
+                {
+                    return (type,
+                        (TransferMethodAttribute)Attribute.GetCustomAttribute(
+                            type, typeof(TransferMethodAttribute)));
+                }
+
+                IColorTransfer GetInstance(Type type)
+                {
+                    var ctor = type.GetConstructor(new Type[0]);
+                    return ctor.Invoke(new object[0]) as IColorTransfer;
                 }
             }
 
-            return types.Keys;
+            return types.AsReadOnly();
         }
 
-        private static readonly Dictionary<string, Type> types
-            = new Dictionary<string, Type>();
+        private static readonly List<ColorTransferDescriptor> types
+            = new List<ColorTransferDescriptor>();
     }
 }
